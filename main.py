838adffaf8869848c6658c72639a199006c192e6
MiquelTactile <miquel@tactile.dk>
2023-04-11 21:13:20 +0200

    Last update for Fiber T7.3

--- main.py
+++ main.py
@@ -17,15 +17,29 @@ bridgewatch_index = "2004"
 fortSterling_index = "4002"
 black_market_index = "3003"
 caerleon_market_index = "3005"
-brecilien_index = "5003"
 arthursRest_index = "4300"
 merlynsRest_index = "1012"
 morganasRest_index = "0008"
 
-locations = f"?locations={thetford_index}, {martlock_index},{lymhurst_index},{bridgewatch_index},{fortSterling_index},{black_market_index}, {caerleon_market_index}, {brecilien_index}, {arthursRest_index},{merlynsRest_index},{morganasRest_index}"
+locations = f"?locations={thetford_index}, {martlock_index},{lymhurst_index},{bridgewatch_index},{fortSterling_index},{black_market_index}, {caerleon_market_index}, {arthursRest_index},{merlynsRest_index},{morganasRest_index}"
 qualities = "&qualities=1"
 
-items = "T6_LEATHER"
+
+def tier(unit):
+    if unit == 0:
+        return ' '
+    elif unit == 1:
+        return "_LEVEL1@1"
+    elif unit == 2:
+        return "_LEVEL2@2"
+    elif unit == 3:
+        return "_LEVEL3@3"
+    elif unit == 4:
+        return "_LEVEL4@4"
+
+
+level = 3
+items = f"T7_FIBER_LEVEL3@3"
 
 
 def run_script():
@@ -36,12 +50,13 @@ def run_script():
     get_daily_json = create_call(daily_call)
 
     export_to_excel(get_average_json, "datafile_average.xlsx")
-    export_to_excel(get_daily_json, "datafile_last_days.xlsx")
+    response = []
 
-    # Items_count = [i["item_count"] for i in get_daily_json["data"][0]]
-    # Locations = get_daily_json["location"]
-    # print(Items_count)
-    # print(Locations)
+    # plt.plot(x, y)
+    # plt.xlabel('X-axis')
+    # plt.ylabel('Y-axis')
+    # plt.title("A simple line graph")
+    # plt.show()
 
 
 def create_average_call(call):
@@ -55,7 +70,7 @@ def create_call(call):
 def export_to_excel(df_json, file_name):
     writer = pd.ExcelWriter(f"{file_name}", engine="xlsxwriter")
     df_json.to_excel(writer, sheet_name='welcome', index=False)
-    writer.save()
+    writer._save()
 
 
 def graph_json_file(df_json):
